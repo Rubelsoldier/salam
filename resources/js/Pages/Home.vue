fd7cc43fb5ac58515a941440e027cdf6f9c0b8eb
Rubel <31316317+Rubelsoldier@users.noreply.github.com>
2024-05-08 20:37:07 +0600

    schollable in both group,post and follower place

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -14,13 +14,13 @@ defineProps({
     <Head title="Salam" />
 
     <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+        <div class="lg:col-span-3 lg:order-1 h-full overflow-auto">
             <GroupList />
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+        <div class="lg:col-span-3 lg:order-3 h-full overflow-auto">
             <FollowingList />
         </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+        <div class="lg:col-span-6 lg:order-2 h-full overflow-auto flex flex-col">
             <CreatePost />
             <PostList class="flex-1"/>
         </div>
