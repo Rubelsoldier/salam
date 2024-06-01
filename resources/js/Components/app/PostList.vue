fd48d828b89385ef3c9542e71da13d522debda4e
rubelranaa <31316317+Rubelsoldier@users.noreply.github.com>
2024-06-01 21:28:38 +0600

    Implementing load more functionality on posts

--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="overflow-auto">
-        <PostItem v-for="post of posts" :key="post.id" 
+        <PostItem v-for="post of allPosts.data" :key="post.id" 
                   :post="post" 
                   @editClick="openEditModal"
                   @attachmentClick="openAttachmentPreviewModal"
         />
+
+        <div ref="loadMoreIntersect"></div>
 
         <PostModal :post="editPost" v-model="showEditModal" @hide="onModalHide"/>
         <AttachmentPreviewModal :attachments="previewAttachmentsPost.post?.attachments || []"
@@ -16,13 +18,10 @@
 <script setup>
 import PostItem from "@/Components/app/PostItem.vue";
 import PostModal from "@/Components/app/PostModal.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {usePage} from "@inertiajs/vue3";
 import AttachmentPreviewModal from "@/Components/app/AttachmentPreviewModal.vue";
-
-defineProps({
-    posts: Array
-});
+import axiosClient from "@/axiosClient.js";
 
 const authUser = usePage().props.auth.user;
 const showEditModal = ref(false)
@@ -30,6 +29,19 @@ const showAttachmentsModal = ref(false)
 const editPost = ref({})
 const previewAttachmentsPost = ref({})
 
+const loadMoreIntersect = ref(null)
+const page = usePage();
+
+const allPosts = ref({
+    data: page.props.posts.data,
+    next: page.props.posts.links.next
+})
+
+const props = defineProps({
+    posts: Array
+})
+
+// Methods 
 function openEditModal(post) {
     editPost.value = post;
     showEditModal.value = true;
@@ -50,6 +62,27 @@ function openAttachmentPreviewModal(post, index) {
     }
     showAttachmentsModal.value = true;
 }
+
+function loadMore() {
+    if (!allPosts.value.next) {
+        return;
+    }
+
+    axiosClient.get(allPosts.value.next)
+        .then(({data}) => {
+            allPosts.value.data = [...allPosts.value.data, ...data.data]
+            allPosts.value.next = data.links.next
+        })
+}
+
+onMounted(() => {
+    const observer = new IntersectionObserver(
+        (entries) => entries.forEach(entry => entry.isIntersecting && loadMore()), {
+            rootMargin: '-250px 0px 0px 0px'
+        })
+
+    observer.observe(loadMoreIntersect.value)
+})
 
 </script>
 
