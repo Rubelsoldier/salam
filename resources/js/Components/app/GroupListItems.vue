dbae0940e374fb0fdc4e66e9c4e8abc8eb28df1e
rubelranaa <31316317+Rubelsoldier@users.noreply.github.com>
2024-06-02 17:27:05 +0600

    Give proper position of new group button
    Changing new group button from group list to grouplistItems component

--- resources/js/Components/app/GroupListItems.vue
+++ resources/js/Components/app/GroupListItems.vue
@@ -1,13 +1,23 @@
 <script setup>
 import TextInput from "@/Components/TextInput.vue";
 import GroupItem from "@/Components/app/GroupItem.vue";
-import {ref} from "vue";
+import GroupModal from "@/Components/app/GroupModal.vue";
+import { ref } from "vue";
+
+const showNewGroupModal = ref(false)
 
 const searchKeyword = ref('')
 </script>
 
 <template>
-    <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full mt-4"/>
+    <div class="flex gap-2  mt-4">
+        <TextInput :model-value="searchKeyword" placeholder="Type to search" class="w-full"/>
+        <button @click="showNewGroupModal = true"
+                class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 w-[120px]">
+            new group
+        </button>
+    </div>
+
     <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
         <div v-if="false" class="text-gray-400 text-center p-3">
             You are not joined to any groups
@@ -117,6 +127,9 @@ const searchKeyword = ref('')
                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
         </div>
     </div>
+    
+    <GroupModal v-model="showNewGroupModal" />
+
 </template>
 
 <style scoped>
