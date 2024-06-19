f44c11c5d8bf47cc5a6717460da9e43724094ae2
rubelranaa <31316317+Rubelsoldier@users.noreply.github.com>
2024-06-19 14:17:13 +0600

    dropdown menu / links dark mode correction
    the links of this menu wasn't working change color in darkmode

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -35,7 +35,7 @@
                                         <span class="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150 "
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
                                             >
                                                 {{ authUser.name }}
 
@@ -181,6 +181,5 @@ function toggleDarkMode(){
         localStorage.setItem('darkMode', '1')
     }
 }
-
 
 </script>
